e44772e4f1f9596ad67d0b4287ba8bed20cd3293
Javier Cuamatzi <fjavier.cuamatzi@gmail.com>
2024-02-06 12:43:09 -0600

    se agrega modulo cursos

---
+++ my-app/microservice-course/src/test/java/mx/com/cuamatziinc/course/MicroserviceCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.cuamatziinc.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
